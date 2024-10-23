39552625dc5b7cd71ce6701768636e8e09abbbf6
Akash Sharma <akashs3468@gmail.com>
2024-10-24 02:37:47 +0530

    database connection changes

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
+// module.exports = {
+//     db: {
+//         username: 'booking_db_pfaz_user',
+//         password: 'Krf45npc9z0tDq3nMGlzCklycirQs4uX',
+//         database: 'booking_db_pfaz',
+//         host: 'postgresql://booking_db_pfaz_user:Krf45npc9z0tDq3nMGlzCklycirQs4uX@dpg-csclv4d6l47c73cpptq0-a.singapore-postgres.render.com',
+//         dialect: 'postgres'
+//     },
+// };
 module.exports = {
     db: {
         username: 'booking_db_pfaz_user',
         password: 'Krf45npc9z0tDq3nMGlzCklycirQs4uX',
         database: 'booking_db_pfaz',
-        host: 'postgresql://booking_db_pfaz_user:Krf45npc9z0tDq3nMGlzCklycirQs4uX@dpg-csclv4d6l47c73cpptq0-a.singapore-postgres.render.com/booking_db_pfaz',
-        dialect: 'postgres'
+        host: 'dpg-csclv4d6l47c73cpptq0-a.singapore-postgres.render.com',
+        dialect: 'postgres',
+        port: 5432 // Make sure to include the port if it's not default.
     },
 };
